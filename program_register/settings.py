b6efdc495cb75bcb66943bee9f1975e50b8c987c
bluebearwhale <58324171+bluebearwhale@users.noreply.github.com>
2019-11-30 11:45:16 +0900

    Update settings.py

--- program_register/settings.py
+++ program_register/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7vwq6*_y_s%(@+ec@$$n+1st=t3&=w$)y826)et4-!hj-jazx!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://ec2-52-79-239-177.ap-northeast-2.compute.amazonaws.com']
 
 
 # Application definition
